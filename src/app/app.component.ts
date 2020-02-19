e368d90bf8408bf4c5436715954ec2e188740fea
Zune Zune <seinmama@amdon.com>
2020-02-19 09:33:42 +0630

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component,HostListener, OnInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'landingNavScrollingActive';
+
+
+  ngOnInit(){
+
+  }
+
+
+  menuClick(e){
+    console.log('reach')
+    console.log(e);
+  }
 }
